3e03cf5eacda0d3c78885586915958f411b0e2a9
alymomar75 <alymomardiallo75@gmail.com>
2026-02-12 15:43:08 +0000

    Create a Streamlit app for grade average calculation
    Implement a Streamlit app to calculate the average of two student grades.

---
+++ app.py
@@ -0,0 +1,9 @@
+#Écrire un programme qui saisit deux notes d’un étudiant calcul et affiche la moyenne des notes.
+
+import streamlit as st 
+num1 =  st.number_input(" num1 : ")
+num2 =  st.number_input(" num2 : ")
+somme = num1 + num2
+
+if st.button("résultat"):
+  st.info( somme )
